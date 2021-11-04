523258f86783858a54068b5279676af7a17eba24
CFMCGEE <funchbusiness@gmail.com>
2021-11-04 00:48:50 -0400

    What I got so far

---
+++ src/main/java/mysql_fundamentals/mysql_definition.java
@@ -0,0 +1,43 @@
+package mysql_fundamentals;
+
+public class mysql_definition {
+
+     /*
+
+                                    Important Stuff
+                                    - Relational Database is a collection of data items organized as a set that includes tables and keys.
+                                    - Open Source
+
+                                    MySQL Workbench
+                                    - SQL Development
+                                     - Open Database Connection
+                                     - Explore Database
+                                     - Execute SQL Statements
+                                    - Data Modeling
+                                     - ERR Model
+                                     - Reverse Engineering
+                                    - Server Administration
+                                     - [Start or Stop] MySQL Server
+                                     - MySQL Monitor
+
+                                    Client/Server System
+                                    - Clients are like PCs, Macs, laptops, tables, smartphones, etc.
+                                    - Server stores files and database (database server)
+                                    - Networks are communications links between client and server
+                                     - It also includes other interface components like hubs and routers.
+                                     - Networking components connect server with network storage
+                                     - Clients and servers typically are on the same network (known as LAN or local area network)
+                                     - If there are more than two LANs located in separate geography location, that's called a WAN or wide area network
+
+                                    Web Application Server
+                                    - ISP (Internet Server Provider)
+                                    - The front end of any application is stored in the application server
+                                    - Every request for data goes into a database server
+                                     - Database servers have a DBMS or database management system.
+
+
+
+      */
+
+
+}
