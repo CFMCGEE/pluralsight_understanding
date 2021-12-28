52f2d8aae273b0b84a63a16733fdb3522fb43368
CFMCGEE <funchbusiness@gmail.com>
2021-12-28 17:30:26 -0500

    Docker and Kubernetes Understanding

---
+++ src/main/java/containers/Containers.java
@@ -0,0 +1,128 @@
+package containers;
+
+public class Containers {
+
+      /*
+
+                                    Containers
+                                    - Containers are like lightweight virtual machines
+                                    - Server (VM, OS, etc)
+                                     - One operating system
+                                      - 4 containers (slices of the operating system)
+                                      - One app per container
+                                    - Individual Preparedness
+                                     - Learning
+                                     - Getting your hands dirty
+                                    - Suitable Workloads
+                                     - Stateful
+                                      - Has to remember stuff (e.x Database)
+                                      - Needs to carry over information
+                                      - Added features for stateful apps
+                                     - Stateless
+                                      - Doesn't remember stuff
+                                    - Adaptable
+                                    - Modern
+                                    - Scalable
+                                    - Self-healing
+                                    - Portable
+                                    - Ecosystem
+                                     - Thriving
+                                     - Lots of products
+                                     - Some help with enhancing Docker and Kubernetes
+                                    - Orchestration
+                                     - Orchestrator
+                                     - App manifest
+                                     - Services
+                                     - Apps comprise
+                                      - Multiple parts (services)
+                                      - Multiple requirements
+                                     - Game plan
+                                      - Describe the app (HTTPS, Load Balancer, Authorization (Auth), etc)
+                                     - Document Game Plan in version control system
+                                     - Key to automation
+                                      - Ordered startups
+                                      - Intelligent scheduling
+                                      - Much more...
+                                     - Give Game Plan to orchestrator (such as Kubernetes/K8s)
+                                     - Let orchestrator deploy and manage app
+
+                                     [ Docker
+
+                                      - Docker, Inc.
+                                       - Enterprise Edition (EE)
+                                        - Open source
+                                        - Costs money
+                                        - Official support
+                                        - Patches
+                                        - Cautious release cycle
+                                        - Stability
+                                        - Steady releases
+                                        - Enterprise featureset
+                                        - Extras
+                                         - Enterprise web UI
+                                         - Security features
+                                         - AD/LDAP
+                                         - Private registry
+                                         - Policies
+                                         - FIPS (https://en.wikipedia.org/wiki/FIPS_(computer_program)#:~:text=FIPS%20(First%20nondestructive%20Interactive%20Partition,(FAT)%20hard%20disk%20partitions.)
+                                         - Workflows & Pipelines
+                                       - Docker Desktop
+                                        - FREE!
+                                       - Enterprise platform
+                                       - Enterprise support
+                                      - Docker... the Technology
+                                       - Makes running our apps inside containers easy
+                                       - Community Edition (CE)
+                                        - Open source (& free)
+                                        - Quick release cycle
+                                        - Edge channel (iffy)
+                                      - Services hold their own container(s)
+                                      - Docker Image
+                                       - An image is like a stopped container
+                                       - Apps in containers
+
+                                     [ Kubernetes
+
+                                      - Managing containerized applications
+                                      - Came from Google
+                                      - Cloud
+                                       - Hosted (canned) options
+                                        - AWS EKS (Amazon Web Services - Elastic Kubernetes Service)
+                                        - Azure AKS (Azure Kubernetes Service)
+                                        - Google GKE (Google Kubernetes Engine)
+                                      - On Premises
+                                       - Build your own
+                                      - Open source
+                                      - Super big scope (HUGE)
+                                      - Features go through well-defined stages
+                                       - Alpha
+                                        - Off by default
+                                        - Early code
+                                        - Uncertain future
+                                        - Not for production
+                                       - Beta
+                                        - On by default
+                                        - Becoming stable
+                                        - Promising future
+                                        - Some details may change
+                                       - GA (https://kubernetes.io/docs/reference/command-line-tools-reference/feature-gates/)
+                                        - Production ready*
+                                        - Solid future
+                                        - Stable
+                                         - Code
+                                         - Features
+                                      - Scheduling
+                                      - Scaling
+                                       - Automatically
+                                      - Healing
+                                      - Updating
+                                      - Clusters
+                                      - Any environment allowed for development
+                                      - Decouples the apps from any environments
+
+                                     ]]
+
+      */
+
+
+}
