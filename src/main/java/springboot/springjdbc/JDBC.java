5f1cd313b7a4f20286b6af0574d99d5abb4ae370
CFMCGEE <funchbusiness@gmail.com>
2021-12-29 14:24:14 -0500

    JDBC Understanding

---
+++ src/main/java/springboot/springjdbc/JDBC.java
@@ -0,0 +1,24 @@
+package springboot.springjdbc;
+
+public class JDBC {
+
+                    /*
+
+                                    Java Database Connectivity (JDBC)
+                                    - Framework
+                                    - Pattern based
+                                    - Injected Resources
+                                    - ORM (https://en.wikipedia.org/wiki/Object%E2%80%93relational_mapping)
+                                    - Can work with existing databases
+                                    - Reduces complexity
+                                    - OOP Design
+                                    - Portable
+                                    - Business Forced
+                                     - Focusing
+                                    - Removes configuration
+                                    - Testing capability
+                                    - Dependency Injection
+
+      */
+
+}
