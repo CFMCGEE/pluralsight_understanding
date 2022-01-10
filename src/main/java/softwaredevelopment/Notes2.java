7484f43ed32dc3bf0f2233ed1a034e527894deeb
CFMCGEE <funchbusiness@gmail.com>
2022-01-10 15:01:13 -0500

    Notes

---
+++ src/main/java/softwaredevelopment/Notes2.java
@@ -0,0 +1,92 @@
+package softwaredevelopment;
+
+public class Notes2 {
+
+         /*
+
+                                            Software Development (even more)
+                                            - System Software
+                                             - Provides core functions such as operating systems, disk management, utilities, hardware management and other operational necessities.
+                                            - Programming Software
+                                             - Gives programmers tools such as text editors, compilers, linkers, debuggers and other tools to create code.
+                                            - Application Software
+                                              - Helps users perform tasks.
+                                              - Office productivity suites
+                                              - Data management software
+                                              - Media players
+                                              - Security programs
+                                              - Applications also refers to web and mobile applications.
+                                             - Embedded software
+                                              - This is used to control machines and devices not typically considered computers (e.g. Telecommunication networks, cars, industrial robots, etc.)
+                                             - Programmers (Coders)
+                                              - They write source code to program computers for specific tasks. This can range from merging databases to displaying text and graphics.
+                                              - Typically interpret instructions from software developers and engineers and use programming languages like C++ or Java.
+                                             - Software Engineers
+                                              - They apply engineering principles to build software and systems to solve problems.
+                                             - Software Developers
+                                              - They drive the overall software development lifecycle.
+                                              - This includes working across functional teams to transform requirements into features, managing development teams and processes, and conducting software testing and maintenance.
+                                             - Steps in Software Development
+                                              - Selection of Methodology
+                                              - Gathering Requirements
+                                              - Developing a design
+                                              - Building of model
+                                              - Construction of Code
+                                              - Testing
+                                              - Management of configuration and defects
+                                              - Deployment
+                                              - Migration of Data
+                                              - Measurement and management of the project
+                                             - Importance of Software Development
+                                              - The ability to reduce customer inquiries
+                                              - Accelerates development cycles
+                                              - Reduces danger
+                                             - Key Features of EFFECTIVE Software Development
+                                              - Artificial intelligence (AI)
+                                               - Enables software to emulate human decision-making and learning.
+                                               - Things such as neutral networks, machine learning and natural language processing and cognitive capabilities
+                                              - Cloud Development
+                                               - Cloud-native
+                                                - This is a way to build applications to exploit cloud environments.
+                                                - This typically consists of discrete, reusable components better known as Microservices
+                                                - Providing architecture styles to cloud environments
+                                               - Cloud-based
+                                                - Provides fast, flexible and cost-efficient integration development environments (IDE)
+                                                - Sometimes known as PaaS (Platform as a Service)
+                                                - These types of environment support coding, design and integration, testing among other development functions
+                                              - Blockchain
+                                               - Reduces costs
+                                               - Frees capital
+                                               - Accelerates processes
+                                              - Low code
+                                               - Production, sometimes cloud services, for application development that focus on employing visual, declarative techniques.
+                                               - Low cost if any for payment and training
+                                               - Reduces the need for coding
+                                               - Enables non-coders to build or help build applications at a lower cost
+                                              - Analytics
+                                               - Lots of positions are open (specifically well over 700k)
+                                              - Model Based Systems Engineering (MBSE)
+                                               - Within MBSE, software modeling languages are used to perform early prototyping, simulation and analysis of software designs for early validation
+                                               - Improves analyze and make more clear the requirements of a project from design to implementation
+                                              - Mobile
+                                               - Used to make mobile applications
+                                              - Agile Development
+                                               - Breaks down requirements into consumable functions while delivering rapidly with those functions to go with their incremental development
+                                               - Feedback loop to help fix issues
+                                              - Capability Maturity Model (CMM)
+                                               - Analyzes the proficiency of software development processes.
+                                               - Tracks progress from AD HOC actions to defined steps to measure results and optimized processes.
+                                              - DevOps
+                                               - A bit of a combination of development and operations, similar to the agile based approach.
+                                              - Rapid application development (RAD)
+                                               - A non-liner approach that condenses design and code construction into one interconnected step
+                                              - Scaled Agile Framework (SAFe)
+                                               - Provides a way to scale agile methodology to a large software development team or organization
+                                              - Waterfall
+                                               - Considered the traditional software development methodology
+                                               - This is a set of cascading linear steps from planning and requirements gathering through development and maintenance
+
+      */
+
+
+}
